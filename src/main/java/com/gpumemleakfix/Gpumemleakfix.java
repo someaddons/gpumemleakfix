52aabffe35ba97ead21831197962016ea003f963
someaddons <someaddons@github.com>
2023-06-23 23:39:32 +0200

    cleanup

--- src/main/java/com/gpumemleakfix/Gpumemleakfix.java
+++ src/main/java/com/gpumemleakfix/Gpumemleakfix.java
@@ -1,7 +1,5 @@
 package com.gpumemleakfix;
 
-import com.gpumemleakfix.config.Configuration;
-import com.gpumemleakfix.event.EventHandler;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -12,18 +10,14 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.Random;
-
 import static com.gpumemleakfix.Gpumemleakfix.MOD_ID;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(MOD_ID)
 public class Gpumemleakfix
 {
-    public static final String        MOD_ID = "gpumemleakfix";
-    public static final Logger        LOGGER = LogManager.getLogger();
-    private static      Configuration config = null;
-    public static       Random        rand   = new Random();
+    public static final String MOD_ID = "gpumemleakfix";
+    public static final Logger LOGGER = LogManager.getLogger();
 
     // GL_OUT_OF_MEMORY
     // GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT
@@ -32,7 +26,6 @@ public class Gpumemleakfix
     public Gpumemleakfix()
     {
         ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> "", (a, b) -> true));
-        Mod.EventBusSubscriber.Bus.FORGE.bus().get().register(EventHandler.class);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
     }
@@ -47,16 +40,5 @@ public class Gpumemleakfix
     private void setup(final FMLCommonSetupEvent event)
     {
         LOGGER.info(MOD_ID + " mod initialized");
-    }
-
-    public static Configuration getConfig()
-    {
-        if (config == null)
-        {
-            config = new Configuration();
-            config.load();
-        }
-
-        return config;
     }
 }
